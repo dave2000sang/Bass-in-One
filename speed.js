46b219f8d41dd2ff7c3b49feb7e70e378289e25d
Dave2000sang <david2000sang@hotmail.com>
2019-08-04 13:44:59 -0400

    add video speed control ',' '.' 'r'

--- speed.js
+++ speed.js
@@ -1,13 +1,13 @@
-
 console.log("SPEED SCRIPT LOADED")
 
 var video = document.getElementsByTagName('video')[0]
 
-
-video.playbackRate = 0.5
-console.log("video plabackRate = " + video.playbackRate)
-
-video.addEventListener('ratechange', function(e) {
-	console.log(e.target.playbackRate)
+document.addEventListener('keydown', e => {
+	if (e.key === ".") {
+		video.playbackRate += 0.2
+	} else if (e.key === ",") {
+		video.playbackRate -= 0.2
+	} else if (e.key === "r") {
+		video.playbackRate = 1.00
+	}
 })
-
